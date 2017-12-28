315a5e6daa6c2131761f57d221a5d3b4c4d3e905
Hikaru Wada <watyaa@gmail.com>
2017-12-28 14:42:51 +0900

    Possible to download all translation files to dest dir

--- lib/fastlane/plugin/onesky/actions/onesky_download_action.rb
+++ lib/fastlane/plugin/onesky/actions/onesky_download_action.rb
@@ -8,9 +8,22 @@ module Fastlane
         client = ::Onesky::Client.new(params[:public_key], params[:secret_key])
         project = client.project(params[:project_id])
 
-        UI.success "Downloading translation '#{params[:locale]}' of file '#{params[:filename]}' from OneSky to: '#{params[:destination]}'"
-        resp = project.export_translation(source_file_name: params[:filename], locale: params[:locale])
-        File.open(params[:destination], 'w') { |file| file.write(resp) }
+        files = JSON.parse(project.list_file)['data'].map { |x| x['file_name'] }
+
+        JSON.parse(project.list_language)['data'].each do |x|
+          if (x['is_ready_to_publish'])
+            locale = x['custom_locale'] || x['code']
+            files.each do |file_name|
+              dest_dir = "#{params[:destination]}/#{locale}.lproj"
+
+              UI.success "Downloading translation #{x['english_name']} (#{x['code']}) of file #{file_name} from OneSky to: #{dest_dir}"
+              resp = project.export_translation(source_file_name: file_name, locale: x['code'])
+
+              FileUtils.mkdir_p(dest_dir) unless File.exists?(dest_dir)
+              File.open("#{dest_dir}/#{file_name}", 'w') { |file| file.write(resp) }
+            end
+          end
+        end
       end
 
       def self.description
@@ -46,25 +59,9 @@ module Fastlane
                                        verify_block: proc do |value|
                                          raise "No project id given, pass using `project_id: 'id'`".red unless value and !value.empty?
                                        end),
-          FastlaneCore::ConfigItem.new(key: :locale,
-                                       env_name: 'ONESKY_DOWNLOAD_LOCALE',
-                                       description: 'Locale to download the translation for',
-                                       is_string: true,
-                                       optional: false,
-                                       verify_block: proc do |value|
-                                         raise 'No locale for translation given'.red unless value and !value.empty?
-                                       end),
-          FastlaneCore::ConfigItem.new(key: :filename,
-                                       env_name: 'ONESKY_DOWNLOAD_FILENAME',
-                                       description: 'Name of the file to download the localization for',
-                                       is_string: true,
-                                       optional: false,
-                                       verify_block: proc do |value|
-                                         raise "No filename given. Please specify the filename of the file you want to download the translations for using `filename: 'filename'`".red unless value and !value.empty?
-                                       end),
           FastlaneCore::ConfigItem.new(key: :destination,
                                        env_name: 'ONESKY_DOWNLOAD_DESTINATION',
-                                       description: 'Destination file to write the downloaded file to',
+                                       description: 'Destination directory to write the downloaded file to',
                                        is_string: true,
                                        optional: false,
                                        verify_block: proc do |value|
